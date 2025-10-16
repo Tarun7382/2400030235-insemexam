e1825835b7289b89b41404e46c65f6633e8f0af8
Bondala Pati Tarun <2400030235@kluniversity.in>
2025-10-16 13:04:47 +0530

    Update App.jsx

--- insem-weatherapp/src/App.jsx
+++ insem-weatherapp/src/App.jsx
@@ -4,8 +4,6 @@ export default function Weather() {
   const [city, setCity] = useState('');
   const [data, setData] = useState(null);
 
-  const API_KEY = '4f9d3a1b7e6c9a5e23f3c1e33b8a6c5b'; // replace with your key
-
   const getWeather = async () => {
     const res = await fetch(
   `https://api.openweathermap.org/data/2.5/weather?q=London&appid=57e11464b779cc91771d5d802cddf8ad&units=metric`
